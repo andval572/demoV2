f0494d6896393644b939d5e864173356e1e4290c
anderson <andval572@alu.centroafuera.es>
2022-01-31 13:46:29 +0100

    TERMINADO2

--- src/main/kotlin/com/example/demo/Juego1.kt
+++ src/main/kotlin/com/example/demo/Juego1.kt
@@ -3,13 +3,18 @@ package com.example.demo
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 
-data class Juego1 (var N: Int, var PREGUNTA: String, var A: String, var B: String, var C: String, var D: String
-              ){
+data class Juego1 (var N: Int,
+    var PREGUNTA:String,
+    var A: String,
+    var B: String,
+    var C: String,
+    var D: String,){
 
-    override fun toString(): String {
-        val gson = Gson()
-        val json = gson.toJson(this)
-        val jsonObject = JsonObject()
-        return gson.toJson(this)
-    }
+        override fun toString(): String {
+            val gson = Gson()
+            val json = gson.toJson(this)
+            val jsonObject = JsonObject()
+            return gson.toJson(this)
+        }
+
 }
--- src/main/kotlin/com/example/demo/JuegoRepository.kt
+++ src/main/kotlin/com/example/demo/JuegoRepository.kt
@@ -25,10 +25,10 @@ class JuegoRepository1 {
 
             Juego1(1,"¿ De que Pais es la Marca ADIDAS ? ","ESPAÑA","COLOMBIA","JAPON","ALEMANIA"),
             Juego1(2,"¿ De que Pais es el Piloto FERNANDO ALONSO ?","ESPAÑA","JAPON","COLOMBIA","ALEMANIA"),
-            Juego1(3,"¿ En que Pais se hace la mejor COCAINA ?","ESPAÑA","JAPON","COLOMBIA","ALEMANIA"),
+            Juego1(3,"¿ En que Pais se hace la mejor COCAINA ?","ESPAÑA","JAPON","COLOMBIA","ALEMANIA",),
             Juego1(4,"¿ En que Pais se encuentra la ciudad mas Technological del MUNDO?","JAPAN","COLOMBIA","ALEMANIA","ESPAÑA"),
             Juego1(5,"¿ De que Pais es la Marca NIKE ?","EE.UU","BRASIL","CHINA","REINO UNIDO"),
-            Juego1(6,"¿ De que Pais es el Piloto MICHAEL SHUMAHER ? ","JAPON","COLOMBIA","ALEMANIA","ESPAÑA"),
+            Juego1(6,"¿ De que Pais es el Piloto MICHAEL SHUMAHER ? ","JAPON","COLOMBIA","ALEMANIA","ESPAÑA",),
             Juego1(7,"¿ En que Pais esta la ciudad de NEW YORK ? ","EE.UU","COLOMBIA","ALEMANIA","ESPAÑA"),
             Juego1(8,"¿ En que Pais se encuentra la ciudad de la LUZ ? ","JAPON","FRANCIA","ALEMANIA","ESPAÑA"),
             Juego1(9,"¿ De que Pais es la Famosa marca de coches FERRARI ?","JAPON","COLOMBIA","ITALIA","ESPAÑA"),
